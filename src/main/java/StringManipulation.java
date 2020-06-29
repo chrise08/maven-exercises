c8e5442b81d4996465e368c09363cdc4a1737527
Chris Eckert <chris.eckert08@gmail.com>
2020-06-29 16:44:42 -0500

    exercise 3b

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -8,10 +8,15 @@ public class StringManipulation {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter something: ");
 		String userInput = scanner.nextLine();
-		if (StringUtils.isNumeric(userInput)) {
-			System.out.println("What you entered is a number.");
-		} else {
-			System.out.println("What you entered is not a number.");
-		}
+		
+		// determines if input is number or not
+//		if (StringUtils.isNumeric(userInput)) {
+//			System.out.println("What you entered is a number.");
+//		} else {
+//			System.out.println("What you entered is not a number.");
+//		}
+		
+		// flips case of the string
+		System.out.println(StringUtils.swapCase(userInput));
 	}
 }
